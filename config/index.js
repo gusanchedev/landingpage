068984fbdb3e134af00f269630acf42e16dc2d0a
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2021-06-03 10:37:08 -0500

    Fix .env files

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
     port: process.env.PORT,
-    db: process.env.MONDO_DB,
+    db: process.env.MONGO_DB,
     dbUser: process.env.MONGO_USER,
     dbPassword: process.env.MONGO_PASSWORD,
 }
