0a91e00b5a24b30e8fda43e203b81d944c85e7ec
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2021-06-07 14:38:49 -0500

    Fix sendgrid function

--- services/sendgrid.js
+++ services/sendgrid.js
@@ -8,7 +8,6 @@ function sendMailToContact(newContact) {
   const msg = {
     to: email, // Change to your recipient
     from: config.sendGridVerifiedSender, // Change to your verified sender
-    bcc: "gustavosg777@gmail.com",
     subject: "This is Gustavo (Fullstack Developer) from gusanche.dev",
     text: `Hi ${name}.<br>Thanks for your contact via gusanche.dev on ${date_of_message}. I will be glad to help you on your fullstack project, so I will be in contact asap. <br>You wrote:<br><em>${message}</em>`,
     html: `Hi ${name}.<br>Thanks for your contact via gusanche.dev on ${date_of_message}. I will be glad to help you on your fullstack project, so I will be in contact asap. <br>You wrote:<br><em>${message}</em>`,
